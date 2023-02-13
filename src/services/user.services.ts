7fd0390c182ba344f8f2e71a1ce5b88c2f2784a2
agnxsh <iamagnix@gmail.com>
2023-02-13 21:18:02 +0530

    Access and Refresh Token Session Module Completed, along with Error Handling (Part of IAM)

--- src/services/user.services.ts
+++ src/services/user.services.ts
@@ -1,6 +1,9 @@
 import { User } from "../entities/user.entity";
 import { CreateUserInput } from "../schemas/user.schema";
+import redisClient from "../utils/connectRedis";
 import { AppDataSource } from "../utils/data-source";
+import config from "config";
+import { signJwt } from "../utils/jwt";
 
 const userRepository = AppDataSource.getRepository(User);
 
@@ -20,4 +23,20 @@ export const findUserById = async (userId: string) => {
 
 export const findUser = async (query: Object) => {
   return await userRepository.findOneBy(query);
+};
+//Enabling access and refresh token
+export const signTokens = async (user: User) => {
+  //1. Create Session
+  redisClient.set(user.id, JSON.stringify(user), {
+    EX: config.get<number>("redisCacheExpiresIn") * 60,
+  });
+  //2. Create Access Tokens
+  const access_tokens = signJwt({ sub: user.id }, "accessTokenPrivateKey", {
+    expiresIn: `${config.get<number>("accessTokenExpiresIn")}m`,
+  });
+  //3. Manage Refesh Tokens
+  const refresh_tokens = signJwt({ sub: user.id }, "refreshTokenPrivateKey", {
+    expiresIn: `${config.get<number>("refreshTokenExpiresIn")}m`,
+  });
+  return { access_tokens, refresh_tokens };
 };
